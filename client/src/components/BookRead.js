79bbe8a5e497bfa8f8b6379743f7cc2890b1c6a7
natalialexandra <natalia.alexandra195@gmail.com>
2021-02-07 22:00:00 +0100

    read book

--- client/src/components/BookRead.js
+++ client/src/components/BookRead.js
@@ -1,43 +1,43 @@
 import React, { useEffect, useState, useContext } from 'react'
-import { Redirect, Link } from "react-router-dom"
+// import { Redirect, Link } from "react-router-dom"
 import { AuthContext } from "../context/AuthContext"
 import { Document, Page, pdfjs } from "react-pdf"
-// import pdf from '../assets/8ba7955af53e9a57c23b0477526a09c3'
+
 
 export default function BookRead(props) {
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
     const { isAuth, userData, books, setBooks } = useContext(AuthContext)
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
 
-    console.log("PROPS:", props)
-    // console.log("test:", props.params.testvalue)
-    console.log("test:", props.location.state)
-
-    console.log("path:", props.location.state.bookpath)
     let pathname = props.location.state.bookpath
+    // console.log("pathname:", pathname)
 
     const onDocumentLoadSuccess = ({ numPages }) => {
         setNumPages(numPages);
     }
 
-    console.log("book props:", props)
-    console.log("book read:", books)
-    // console.log("BOOK:", book)
+    const clicked = ({ pageNumber }) => alert('Clicked an item from page ' + pageNumber + '!')
 
     return (
         <div>
             <Document
-                // file={`${books.bookFileName}.pdf`}
-                // file={`${pathname}.pdf`}
-                // file="8ba7955af53e9a57c23b0477526a09c3.pdf"
-                // file={pathname}
-                file={`data:application/pdf;base64,${pathname}`}
+                file={pathname}
                 onLoadSuccess={onDocumentLoadSuccess}
+                onItemClick={clicked}
             >
                 <Page pageNumber={pageNumber} />
             </Document>
             <p>Page {pageNumber} of {numPages}</p>
-        </div>
+
+            <button onClick={() => setPageNumber(prevState => (prevState - 1))}>
+                previous page
+            </button>
+            <button onClick={() => setPageNumber(prevState => (prevState + 1))}>
+                Next page
+            </button>
+
+        </div >
     )
 }
