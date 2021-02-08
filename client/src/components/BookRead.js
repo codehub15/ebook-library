6bc8120d2e5683b6ea4b152f96609df60cfa6c58
natalialexandra <natalia.alexandra195@gmail.com>
2021-02-08 20:29:52 +0100

    read book

--- client/src/components/BookRead.js
+++ client/src/components/BookRead.js
@@ -1,43 +1,34 @@
-import React, { useEffect, useState, useContext } from 'react'
-// import { Redirect, Link } from "react-router-dom"
-import { AuthContext } from "../context/AuthContext"
+import React, { useState } from 'react'
 import { Document, Page, pdfjs } from "react-pdf"
-
 
 export default function BookRead(props) {
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
-    const { isAuth, userData, books, setBooks } = useContext(AuthContext)
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
-
     let pathname = props.location.state.bookpath
-    // console.log("pathname:", pathname)
 
     const onDocumentLoadSuccess = ({ numPages }) => {
         setNumPages(numPages);
     }
 
-    const clicked = ({ pageNumber }) => alert('Clicked an item from page ' + pageNumber + '!')
-
     return (
-        <div>
-            <Document
-                file={pathname}
-                onLoadSuccess={onDocumentLoadSuccess}
-                onItemClick={clicked}
-            >
-                <Page pageNumber={pageNumber} />
-            </Document>
-            <p>Page {pageNumber} of {numPages}</p>
-
-            <button onClick={() => setPageNumber(prevState => (prevState - 1))}>
+        <div className="read-book">
+            <button onClick={() => setPageNumber(prevState => (prevState - 1))} className="btn-book-read btn-prev">
                 previous page
             </button>
-            <button onClick={() => setPageNumber(prevState => (prevState + 1))}>
+
+            <button onClick={() => setPageNumber(prevState => (prevState + 1))} className="btn-book-read btn-next">
                 Next page
             </button>
 
+            <Document
+                file={pathname}
+                onLoadSuccess={onDocumentLoadSuccess}
+                className="book-doc"
+            >
+                <Page pageNumber={pageNumber} />
+                <p>Page {pageNumber} of {numPages}</p>
+            </Document>
         </div >
     )
 }
