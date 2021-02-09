5ec12a1730692568322e7a9068dd1c5b615ff029
natalialexandra <natalia.alexandra195@gmail.com>
2021-02-09 11:04:31 +0100

    client/

---
+++ client/build/precache-manifest.05d0f9ba3ebf8468c04e547aaeedb0ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2f5741cfa4124f7be4a24949c68621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13493fd80ac2b81f30ac",
+    "url": "/static/css/main.81cf60c8.chunk.css"
+  },
+  {
+    "revision": "c3eb8a75e040ecae8098",
+    "url": "/static/js/2.1ea1d18e.chunk.js"
+  },
+  {
+    "revision": "04fc7b1b65347ddd91676bc8c092f75a",
+    "url": "/static/js/2.1ea1d18e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13493fd80ac2b81f30ac",
+    "url": "/static/js/main.7ea37fbc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "3a19a0ae3d623542ca2f848eae9d3d2e",
+    "url": "/static/media/8ba7955af53e9a57c23b0477526a09c3.3a19a0ae.bin"
+  },
+  {
+    "revision": "d2850a53279ddebda78d027b0c7d7094",
+    "url": "/static/media/default-cover-book.d2850a53.png"
+  },
+  {
+    "revision": "3c124085f0022f0676a8eef4b829d957",
+    "url": "/static/media/ebook.3c124085.svg"
+  },
+  {
+    "revision": "278e4e0b4c95866ee61c38cbca2fade0",
+    "url": "/static/media/ebooks.278e4e0b.jpg"
+  }
+]);
