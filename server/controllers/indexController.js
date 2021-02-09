c2fad3e74e37423619bb98fe66dc43446c9d38f0
natalialexandra <natalia.alexandra195@gmail.com>
2021-02-09 22:27:04 +0100

    improve code

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,9 +1,7 @@
 const path = require("path")
 
 exports.indexController = (req, res, next) => {
-    console.log("start")
     // res.send("received get request")
-    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     res.sendFile(path.join(__dirname + '../../../client/build/index.html'))
     // res.sendFile(path.resolve(__dirname, "../../client", "build", "index.html"))
 }
